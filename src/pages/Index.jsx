9e0665636c6aba17282bbbb0cb68de2652cd2ed9
Feilong Huang <52912515+feelonewong@users.noreply.github.com>
2024-11-20 17:36:32 +0800

    update: 完成项目

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -3,7 +3,9 @@ import Header from "../widgets/Header.jsx";
 import Hero from "../widgets/Hero.jsx";
 import Solutions from "../widgets/Solutions.jsx";
 import WorkFlow from "../widgets/WorkFlow.jsx";
-
+import Clients from "../widgets/Clients.jsx";
+import Contact from "../widgets/Contact.jsx";
+import Footer from "../widgets/Footer.jsx";
 function Index() {
     return (<div className="container mx-auto p-4">
         <Header/>
@@ -15,8 +17,14 @@ function Index() {
             <section className="mt-20">
                 <WorkFlow/>
             </section>
-
+            <section className="mt-20 mx-auto md:w-3/5">
+                <Clients/>
+            </section>
+            <section className="mt-20">
+                <Contact/>
+            </section>
         </main>
+        <Footer/>
     </div>)
 }
 
